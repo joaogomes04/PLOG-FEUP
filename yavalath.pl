b6e4f819428d929f7928657933cd84aa5758c970
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2016-10-13 10:49:45 +0100

    Added lines legend

--- yavalath.pl
+++ yavalath.pl
@@ -1,12 +1,13 @@
-board([e,e,e,e,e,s,s,s,s,s,e,e,e,e,e],
-      [e,e,e,e,s,s,s,s,s,s,s,e,e,e,e],
-      [e,e,e,s,s,s,s,s,s,s,s,s,e,e,e],
-      [e,e,s,s,s,s,s,s,s,s,s,s,s,e,e],
-      [e,s,s,s,s,s,s,s,s,s,s,s,s,s,e],
-      [e,e,s,s,s,s,s,s,s,s,s,s,s,e,e],
-      [e,e,e,s,s,s,s,s,s,s,s,s,e,e,e],
-      [e,e,e,e,s,s,s,s,s,s,s,e,e,e,e],
-      [e,e,e,e,e,s,s,s,s,s,e,e,e,e,e]
+board([e,e,e,e,e, e,'1','2','3','4','5',e,e,e,e],
+      [e,e,e,e,'A',s,s,s,s,s,'6',e,e,e,e],
+      [e,e,e,'B',s,s,s,s,s,s,s,'7',e,e,e],
+      [e,e,'C',s,s,s,s,s,s,s,s,s,'8',e,e],
+      [e,'D',s,s,s,s,s,s,s,s,s,s,s,'9',e],
+      ['E',s,s,s,s,s,s,s,s,s,s,s,s,s,e],
+      [e,'F',s,s,s,s,s,s,s,s,s,s,s,e,e],
+      [e,e,'G',s,s,s,s,s,s,s,s,s,e,e,e],
+      [e,e,e,'H',s,s,s,s,s,s,s,e,e,e,e],
+      [e,e,e,e,'I',s,s,s,s,s,e,e,e,e,e]
       ).
 
 display_board([L1|Ls]):-
@@ -19,6 +20,11 @@ display_board([]):- nl.
 display_line([E|Es]):-
   translate(E,V),
   write(V),
+  write('|'),
+  display_line(Es).
+
+display_line([E|Es]):-
+  write(E),
   write('|'),
   display_line(Es).
 
