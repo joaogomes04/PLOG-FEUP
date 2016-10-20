4cae4a737ee57d7ba8d8d2138888f0d5928690ef
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2016-10-20 16:34:03 +0100

    changes on visualization

--- yavalath.pl
+++ yavalath.pl
@@ -1,13 +1,16 @@
-board([e,e,e,e,e, e,'1','2','3','4','5',e,e,e,e],
-      [e,e,e,e,'A',s,s,s,s,s,'6',e,e,e,e],
-      [e,e,e,'B',s,s,s,s,s,s,s,'7',e,e,e],
-      [e,e,'C',s,s,s,s,s,s,s,s,s,'8',e,e],
-      [e,'D',s,s,s,s,s,s,s,s,s,s,s,'9',e],
-      ['E',s,s,s,s,s,s,s,s,s,s,s,s,s,e],
-      [e,'F',s,s,s,s,s,s,s,s,s,s,s,e,e],
-      [e,e,'G',s,s,s,s,s,s,s,s,s,e,e,e],
-      [e,e,e,'H',s,s,s,s,s,s,s,e,e,e,e],
-      [e,e,e,e,'I',s,s,s,s,s,e,e,e,e,e]
+:- use_module(library(lists)).
+
+
+board(['               ',' 1 ',' 2 ',' 3 ',' 4 ',' 5 '],
+      ['         ',' A ',s,s,s,s,s,' 6 '],
+      ['       ',' B ',s,s,s,s,s,s,' 7 '],
+      ['     ',' C ',s,s,s,s,s,s,s,' 8 '],
+      ['   ',' D ',s,s,s,s,s,s,s,s,' 9 '],
+      [' ',' E ',s,s,s,s,s,s,s,s,s],
+      ['   ',' F ',s,s,s,s,s,s,s,s],
+      ['     ',' G ',s,s,s,s,s,s,s],
+      ['       ',' H ',s,s,s,s,s,s],
+      ['         ',' I ',s,s,s,s,s]
       ).
 
 display_board([L1|Ls]):-
@@ -15,7 +18,6 @@ display_board([L1|Ls]):-
   display_board(Ls).
 
 display_board([]):- nl.
-
 
 display_line([E|Es]):-
   translate(E,V),
@@ -30,6 +32,7 @@ display_line([E|Es]):-
 
 display_line([]).
 
-
-translate(s, ' ').
-translate(e, '#').
+translate(s, '   ').
+%translate(e, '#').
+translate(w, ' o ').
+translate(b, ' x ').
