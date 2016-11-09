06b5f46dc198bfb271be9c5229f259c309835553
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2016-11-09 13:25:53 +0000

    add a menu, console utilities and did some organization

---
+++ logic.pl
@@ -0,0 +1,21 @@
+:- use_module(library(lists)).
+:- use_module(library(random)).
+
+
+
+initialBoard(['               ',' 1 ',' 2 ',' 3 ',' 4 ',' 5 '],
+      ['         ',' A ',s,s,s,s,s,' 6 '],
+      ['       ',' B ',s,s,s,s,s,s,' 7 '],
+      ['     ',' C ',s,s,s,s,s,s,s,' 8 '],
+      ['   ',' D ',s,s,s,s,s,s,s,s,' 9 '],
+      [' ',' E ',s,s,s,s,s,s,s,s,s],
+      ['   ',' F ',s,s,s,s,s,s,s,s],
+      ['     ',' G ',s,s,s,s,s,s,s],
+      ['       ',' H ',s,s,s,s,s,s],
+      ['         ',' I ',s,s,s,s,s]
+      ).
+
+
+
+%canMovePiece(Board, Letter, Number)
+%movePiece(Bi, Letter, Number, Bf)
