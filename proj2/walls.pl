87daa6355b66047b49508da14a666d7f831bb3f5
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2016-12-18 18:14:58 +0000

    bug fixed

--- proj2/walls.pl
+++ proj2/walls.pl
@@ -3,16 +3,16 @@
 
 notpivot(E) :- var(E); integer(E).
 
-up(M, X-Y, [E|L]) :- Y1 #= Y - 1, nth1(Y1, M, Linha), nth1(X, Linha, E), notpivot(E), up(M, X-Y1, L).
+up(M, X-Y, [E|L]) :- Y1 #= Y - 1, nth1(Y1, M, Linha), nth1(X, Linha, E), notpivot(E), up(M, X-Y1, L),!.
 up(_, _, []).
 
-down(M, X-Y, [E|L]) :- Y1 #= Y + 1, nth1(Y1, M, Linha), nth1(X, Linha, E), notpivot(E), down(M, X-Y1, L).
+down(M, X-Y, [E|L]) :- Y1 #= Y + 1, nth1(Y1, M, Linha), nth1(X, Linha, E), notpivot(E), down(M, X-Y1, L),!.
 down(_, _, []).
 
-left(M, X-Y, [E|L]) :- X1 #= X - 1, nth1(Y, M, Linha), nth1(X1, Linha, E), notpivot(E), left(M, X1-Y, L).
+left(M, X-Y, [E|L]) :- X1 #= X - 1, nth1(Y, M, Linha), nth1(X1, Linha, E), notpivot(E), left(M, X1-Y, L),!.
 left(_, _, []).
 
-right(M, X-Y, [E|L]) :- X1 #= X + 1, nth1(Y, M, Linha), nth1(X1, Linha, E), notpivot(E), right(M, X1-Y, L).
+right(M, X-Y, [E|L]) :- X1 #= X + 1, nth1(Y, M, Linha), nth1(X1, Linha, E), notpivot(E), right(M, X1-Y, L),!.
 right(_, _, []).
 
 constrainpivot(M, X-Y-Pivot) :-
@@ -22,16 +22,22 @@ constrainpivot(M, X-Y-Pivot) :-
   down(M, X-Y, D), !,
   constraint(L, R, U, D, Pivot).
 
-constseq([], _, 0).
-constseq([L|_], A, 0) :- L #\= A.
-constseq([A|Ls], A, N) :- N1 #= N - 1, constseq(Ls, A, N1).
+constseq_(_, [], _, 0).
+constseq_(X, [Y|L], F, N) :-
+  X #= Y #/\ F #<=> B,
+  N #= M + B,
+  constseq_(X, L, B, M).
+
+constseq(L, E, N) :-
+  constseq_(E, L, 1, N).
 
 constraint(L, R, U, D, N) :-
-  NL + NR + NU + ND #= N,
   constseq(L, 0, NL),
+  %write('mbn'),nl,
   constseq(R, 0, NR),
   constseq(U, 1, NU),
-  constseq(D, 1, ND).
+  constseq(D, 1, ND),
+  NL + NR + NU + ND #= N.
 
 findPivots(M, X-Y, P) :-
   nth1(Y, M, R),
@@ -47,6 +53,7 @@ solve(M) :-
   domain(Vs,0,1),
   findall(X-Y-P, findPivots(M, X-Y, P), R),
   maplist(constrainpivot(M), R),
+  %write(Vs),nl,
   labeling([],Vs).
 
 extractvars(M, Vs) :- append(M, F), include(var, F, Vs).
@@ -68,11 +75,9 @@ matrix([[_, _, p(4), _, _, p(1)],
        [_, _, p(1), _, _, p(2)],
        [_, _, _, _, p(2), _],
        [p(2), _, _, p(3), _, _]]).
-  /* M = [[p(1), _, p(1)],
-       [_, _, _],
-       [_, _, _]], */
+       
 solvePuzzle(M):-
   matrix(M),
   solve(M),
   printmatrix(M),
-  write(M).
+  write(M), nl, nl.
